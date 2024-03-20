816682502e241adfa982eb8c6fe661a325af2e24
Sr-Croqueta <carlitospd8@gmail.com>
2024-03-20 11:23:12 +0100

    4ty4

--- src/app/selector/selector.component.ts
+++ src/app/selector/selector.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-selector',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './selector.component.html',
   styleUrl: './selector.component.css'
 })
 export class SelectorComponent {
+ prb: any="dfhrh";
+ 
+  constructor(){
+     console.log(this.prb)
+  }
+
+ 
 
 }
+
+
