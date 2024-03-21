e7d002acd10dbe18306ea332edc91a212da959a1
Sr-Croqueta <carlitospd8@gmail.com>
2024-03-21 08:36:56 +0100

    ghsfg

--- reservaaulas/routes/web.php
+++ reservaaulas/routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CentroController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,7 +16,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/centros', [CentroController::class, 'index'])->name('centro.index');
 
 require __DIR__.'/auth.php';
