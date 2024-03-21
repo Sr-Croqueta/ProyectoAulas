1b1036a0a55ddcfbc9d55d79213fdeae8065ae16
Sr-Croqueta <carlitospd8@gmail.com>
2024-03-21 10:41:47 +0100

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 import { SelectorComponent } from './selector/selector.component';
-export const routes: Routes = [ {path: "",redirectTo:"Seleccion", pathMatch:"full"},{path:"Seleccion",component:SelectorComponent}];
+import { LoginComponent } from './login/login.component';
+export const routes: Routes = [ {path: "",redirectTo:"Seleccion", pathMatch:"full"},{path:"Seleccion",component:SelectorComponent},{path:"login",component:LoginComponent}];
+
