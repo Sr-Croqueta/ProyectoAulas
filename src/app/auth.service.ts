168f378357d259d3cfd44715987158aa23495a57
Sr-Croqueta <carlitospd8@gmail.com>
2024-03-21 11:32:52 +0100

    dfg

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -12,7 +12,7 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(email: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/login`, { email, password }).pipe(
+    return this.http.post<any>(`${this.apiUrl}`, { email, password }).pipe(
       tap(response => {
         // Almacena el token de acceso en el almacenamiento local
         localStorage.setItem('access_token', response.access_token);
