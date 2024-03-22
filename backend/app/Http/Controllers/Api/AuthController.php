43e532c7ac30d5aeb00730336e0e417b8ed30acf
Sr-Croqueta <carlitospd8@gmail.com>
2024-03-22 08:50:29 +0100

    yjety

---
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|confirmed'
+        ]);
+
+        $user = new User();
+        $user->name=$request->name;
+        $user->email=$request->email;
+        $user->password=Hass::make($request->password);
+        $user->save();
+        
+
+        return response($user,Response::HTTP_CREATED);
+
+        
+
+    }
+
+    public function login(Request $request){
+
+        $credentials=$request->validate([
+            'email'=>['required','email'],
+            'password'=>'required'
+        ]);
+
+        if(Auth::attempt([$credentials]) ){
+            $user = Auth::user();
+            $token = $user->createToken('token')->palinTextToken;
+            $cookie = cookie('cookie_token',$token,60*24);
+            return response(["token"=>$token],Reponse::HTTP_OK)->withoutCookie($cookie);
+
+        }else{
+            return response(["message"=>"Credenciales Inválidas"],Response::HTTP_UNAUTHORIZED);
+        }
+
+    }
+
+    public function userProfile(Request $request){
+
+        return response()->json([
+            "message"=> "userProfile OK",
+            "message"=> auth()->user()
+        ],Response::HTTP_OK);
+
+        
+    }
+
+    public function logout(){
+
+        
+    }
+
+    public function allUsers(){
+
+        
+    }
+    
+}
